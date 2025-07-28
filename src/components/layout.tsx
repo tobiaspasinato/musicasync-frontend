63283e4f12377b526fa722794f809c53f78add92
tobiaspasinato <tobiasmarpasinato@gmail.com>
2025-07-27 23:00:38 -0300

    add button

---
+++ src/components/layout.tsx
@@ -0,0 +1,16 @@
+import React, { type ReactNode } from 'react';
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className='bg-black min-h-screen flex flex-col w-screen'>
+      {/* layout content */}
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
